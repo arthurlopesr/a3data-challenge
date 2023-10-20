9f44dcc67ccf1f31ad9e77a6e0ce9a011f126f9e
Arthur Lopes <arthurlopr12@gmail.com>
2023-10-19 22:15:00 -0300

    fix: handling error

--- frontend/src/services/SurgicalOrdersService.ts
+++ frontend/src/services/SurgicalOrdersService.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from "axios";
 import { SurgicalOrdersParams } from "../@types/surgical-orders";
 import { httpClient } from "./httpClient";
 
@@ -5,17 +6,32 @@ type SurgicalOrdersResponse = Array<SurgicalOrdersParams>;
 
 export class SurgicalOrdersService {
   static async findAllOrderSurgery() {
-    const { data } = await httpClient.get<SurgicalOrdersResponse>(
-      `/surgical-orders`
-    );
-    return data;
+    try {
+      const { data } = await httpClient.get<SurgicalOrdersResponse>(
+        `/surgical-orders`
+      );
+      return data;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        alert("Verifique as se API está em execução");
+      }
+    }
   }
 
   static async createOrderSurgery(data: SurgicalOrdersParams) {
-    await httpClient.post("/surgical-orders", {
-      ...data,
-      surgeryDate: new Date(data.surgeryDate).toISOString(),
-    });
+    try {
+      await httpClient.post("/surgical-orders", {
+        ...data,
+        surgeryDate: new Date(data.surgeryDate).toISOString(),
+      });
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        error.response?.status === 400 &&
+          alert("A observação deve conter no minimo 10 caracteres");
+        error.response?.status === 409 &&
+          alert("Essa sala já está reservada para essa data");
+      }
+    }
   }
 
   static async updateOrderSurgery({ code, ...params }: SurgicalOrdersParams) {
